62fa138ee05c7e9594a8cdee6a431b3e0c69477c
huchong <1178869226@qq.com>
2019-12-18 02:59:25 -0500

    [REVISE] evaluation comparison

--- figures/eval.R
+++ figures/eval.R
@@ -11,7 +11,7 @@ p <- ggplot(eval.m, aes(x= reorder(type, -value), y=value))+
   geom_bar(aes(fill=variable),  position="dodge",stat="identity", width = 0.5)+
   theme(legend.position="left")+                                                                  
   labs(y="accuracy", 
-       x="Different methods evaluations on convensional features and  social media features", 
+       x="", 
        fill = "methods")+
   theme(axis.text.y = element_text(size = 18),
         axis.text.x = element_text(angle = 30, hjust = 1, size = 18),
